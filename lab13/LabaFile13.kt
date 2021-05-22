f8ca7a1a4fd82c8e584abd4cf57bcd2b0f1f90cf
Elizaveta Malezh <milkasps66@gmail.com>
2021-05-22 12:14:19 +0300

    task3
    Поздороваться с пользователем. Применить форматирование строки

--- lab13/LabaFile13.kt
+++ lab13/LabaFile13.kt
@@ -1,4 +1,16 @@
 import java.util.*
 
-//1 задание
-fun main()=println("Hello World!")
+////1 задание
+//fun main()=println("Hello World!")
+
+//3 задание
+fun main(args: Array<String>) {
+    if (args.size == 0) {
+        print("Введите ваше имя:")
+        return
+    }
+    println("Привет, ${args[0]}!")
+    println("Привет, %s!".format(args[0]))
+    println("Привет " + args[0]+ "!")
+    println("Привет,\n${args[0]}\n!")
+}
