b1b9d281ac283ccb6ec1d6888eb6c64105b96448
Elizaveta Malezh <milkasps66@gmail.com>
2021-05-22 12:23:24 +0300

    task4
    Рассмотреть методы класса String

--- lab13/LabaFile13.kt
+++ lab13/LabaFile13.kt
@@ -14,3 +14,16 @@ fun main(args: Array<String>) {
     println("Привет " + args[0]+ "!")
     println("Привет,\n${args[0]}\n!")
 }
+
+//4 задание
+fun main() { //var stroka = readLine()
+    if (stroka != null) { //println("Метод length: ${stroka.length}")
+        println("Метод hashCode: ${stroka.hashCode()}")
+        println("Метод capitalize: ${stroka.capitalize()}")
+        println("Метод chunked: ${stroka.chunked(3)}")
+        println("Метод drop: ${stroka.drop(3)}")
+        println("Метод isEmpty: ${stroka.isEmpty()}")
+        println("Метод plus: ${stroka.plus("ssss")}")
+    }
+}
+
