fef05a16cc88c91b00fc81c683de3108ea7c3f0f
Elizaveta Malezh <milkasps66@gmail.com>
2021-05-23 12:01:06 +0300

    task3
    Написать функции произведение цифр числа, минимальная и
    максимальная
    цифра
    с
    использованием
    рекурсии
    вверх и с использованием хвостовой рекурсии.

--- lab14/LabaFile14.kt
+++ lab14/LabaFile14.kt
@@ -17,3 +17,50 @@ tailrec fun sumDigitTail(num: Int, sum: Int = 0): Int = if (num != 0) {
     sumDigitTail(num / 10,sum + (num % 10))
 } else sum
 
+
+// 3 задание
+fun prodDigitUp(num: Int):Int = if(num!=0) {
+    val prod = prodDigitUp(num / 10)
+    prod * (num % 10)
+} else 1
+
+tailrec fun prodDigitTail(num: Int, prod: Int = 1): Int = if (num != 0) {
+    prodDigitTail(num / 10,prod * (num % 10))
+} else prod
+
+
+fun minDigitUp(num: Int): Int {
+    val newNum = num / 10
+    val digit = num % 10
+
+    return if (newNum != 0) {
+        val min = minDigitUp(newNum)
+        if (digit < min)
+            digit
+        else min
+    }
+    else digit
+}
+
+tailrec fun minDigitTail(num: Int, min: Int = num%10): Int = if (num != 0) {
+    minDigitTail(num / 10, if (num%10<min) num%10 else min)
+} else min
+
+
+fun maxDigitUp(num: Int): Int {
+    val newNum = num / 10
+    val digit = num % 10
+
+    return if (newNum != 0) {
+        val max = maxDigitUp(newNum)
+        if (digit > max)
+            digit
+        else max
+    }
+    else digit
+}
+
+tailrec fun maxDigitTail(num: Int, max: Int = num%10): Int = if (num != 0) {
+    maxDigitTail(num / 10, if (num%10>max) num%10 else max)
+} else max
+
