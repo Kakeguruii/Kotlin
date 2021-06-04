cb3258ac9c6fc08c4d9a0e06e980233eeda4e8c1
Elizaveta Malezh <milkasps66@gmail.com>
2021-06-04 20:18:55 +0300

    task2.1

--- lab16/LabaFile16.kt
+++ lab16/LabaFile16.kt
@@ -18,3 +18,27 @@ tailrec fun obxodString(s: String, inVal: Int = -100000): Int =
         } else obxodString(s.drop(a.length + 1), inVal)
 
     } else inVal
+
+// task2
+
+fun printMass(mas: CharArray, i: Int = 0): String =
+    if (i != mas.size) {
+        println(mas[i])
+        printMass(mas, i + 1)
+    } else "\n"
+
+fun printMass2(mas: IntArray, i: Int = 0): String =
+    if (i != mas.size) {
+        println(mas[i])
+        printMass2(mas, i + 1)
+    } else "\n"
+
+//5
+fun randomCharInString(s: String): String {
+    val listS = s.toList()
+    val newS = listS.shuffled()
+    var res = newS.toString()
+    res = res.removeSurrounding("[", "]")
+    res = res.replace(",", "")
+    return res
+}
