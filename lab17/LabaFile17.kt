40af159d17c1c53f8e284ad01ec2272f3c078dc4
Elizaveta Malezh <milkasps66@gmail.com>
2021-06-08 20:36:59 +0300

    task2

--- lab17/LabaFile17.kt
+++ lab17/LabaFile17.kt
@@ -5,3 +5,36 @@ import kotlin.random.Random
 // task1
 //class PTS(val name:String, val surname:String,val year:Int,val city:String,val id:Int)
 
+// task2
+class Prava (var name: String, val surname: String, val year: Int, val city: String, val id: Int) {
+    fun show() {
+        println("Имя: ${name}")
+        println("Фамилия: ${surname}")
+        println("Год: ${year}")
+        println("Город: ${city}")
+        println("Номер: ${id}")
+        println("\n")
+    }
+
+
+    fun savePrava() {
+        val regName = Regex("""([a-z]|[A-Z]|[а-я]|[А-Я]|[" "])*""")
+        val regSurname = Regex("""([a-z]|[A-Z]|[а-я]|[А-Я]|[" "])*""")
+        val regYear = Regex("""(19|20\d\d)""")
+        val regCity = Regex("""([a-z]|[A-Z]|[а-я]|[А-Я]|[" "])*""")
+        val regId = Regex("""\d\d\d\d\d\d\d\d\d""")
+
+        if (!(this.name.matches(regName)))
+            throw Exception("YareYare1")
+        if (!(this.surname.toString().matches(regSurname)))
+            throw Exception("YareYare2")
+        if (!(this.year.toString().matches(regYear)))
+            throw Exception("YareYare3")
+        if (!(this.city.toString().matches(regCity)))
+            throw Exception("YareYare4")
+        if (!(this.id.toString().matches(regId)))
+            throw Exception("YareYare5")
+    }
+
+}
+
